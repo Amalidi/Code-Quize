0859d91f6946a7b8352f7454f56c4e4177ce6494
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-04-27 12:45:38 +0100

    getting element ids and countdown time banner

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Targeting the banner box
+const bannerBox = document.getElementById("banner-box");
+
+// Targeting the start quiz button
+const startButton = document.getElementById("start-btn")
+
+// Targeting the main section
+const mainSection = document.getElementById("start-btn")
+
+// Question options

81008c830bf837582812911324f5ce90e681f61c
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-04-29 12:56:11 +0100

    added the question section

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,34 @@ const startButton = document.getElementById("start-btn")
 // Targeting the main section
 const mainSection = document.getElementById("start-btn")
 
-// Question options
+
+
+// this part removes banner section 
+const removeBanner = () => {
+    console.log('render banner')
+    bannerBox.remove();
+};
+
+
+// render questions
+const renderQuestionSection = () => {
+    console.log('render question')
+};
 
 // declaring the event handler for the quiz start button click
 const quizStartButtonClick = () => {
     console.log("start button");
+
+// this part removes banner section 
+removeBanner();
+
+
+// render questions
+renderQuestionSection();
 };
 
 // even listner to quiz start button
 startButton.addEventListener("click", quizStartButtonClick)
+
+
+// Question options
